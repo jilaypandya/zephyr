a1567acb151dc1ad23a4e0b43dc06a0c81067b78
Jilay Pandya <jilay.pandya@outlook.com>
2025-05-13 22:48:25 +0200

    drivers: stepper_control: integrate timing source
    This commit integrates timing source from by refactoring it out from
    step_dir lib
    
    Signed-off-by: Jilay Pandya <jilay.pandya@outlook.com>

--- drivers/stepper_control/zephyr_stepper_motion_control/stepper_control.c
+++ drivers/stepper_control/zephyr_stepper_motion_control/stepper_control.c
@@ -9,11 +9,15 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/stepper_control.h>
 
+#include "stepper_motion_control_timing_source.h"
+
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(stepper_control, CONFIG_STEPPER_CONTROL_LOG_LEVEL);
 
 struct stepper_control_config {
 	const struct device *stepper;
+	const struct timing_source_config timing_config;
+	const struct stepper_timing_source_api *timing_source;
 };
 
 struct stepper_control_data {
@@ -25,23 +29,81 @@ struct stepper_control_data {
 	int32_t actual_position;
 	int32_t reference_position;
 	enum stepper_control_mode mode;
-	uint64_t step_interval;
-	struct k_work_delayable stepper_control_dwork;
 	stepper_control_event_callback_t callback;
 	void *event_cb_user_data;
+	struct timing_source_data timing_data;
+#ifdef CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_GENERATE_ISR_SAFE_EVENTS
+	struct k_work event_callback_work;
+	struct k_msgq event_msgq;
+	uint8_t event_msgq_buffer[CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_EVENT_QUEUE_LEN *
+				  sizeof(enum stepper_control_event)];
+#endif /* CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_GENERATE_ISR_SAFE_EVENTS */
 };
+
+void stepper_trigger_callback(const struct device *dev, enum stepper_control_event event)
+{
+	struct stepper_control_data *data = dev->data;
+
+	if (!data->callback) {
+		LOG_WRN_ONCE("No callback set");
+		return;
+	}
+
+	if (!k_is_in_isr()) {
+		data->callback(dev, event, data->event_cb_user_data);
+		return;
+	}
+
+#ifdef CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_GENERATE_ISR_SAFE_EVENTS
+	/* Dispatch to msgq instead of raising directly */
+	int ret = k_msgq_put(&data->event_msgq, &event, K_NO_WAIT);
+
+	if (ret != 0) {
+		LOG_WRN("Failed to put event in msgq: %d", ret);
+	}
+
+	ret = k_work_submit(&data->event_callback_work);
+	if (ret < 0) {
+		LOG_ERR("Failed to submit work item: %d", ret);
+	}
+#else
+	LOG_WRN_ONCE("Event callback called from ISR context without ISR safe events enabled");
+#endif /* CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_GENERATE_ISR_SAFE_EVENTS */
+}
+
+#ifdef CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_GENERATE_ISR_SAFE_EVENTS
+static void stepper_work_event_handler(struct k_work *work)
+{
+	struct stepper_control_data *data =
+		CONTAINER_OF(work, struct stepper_control_data, event_callback_work);
+	enum stepper_control_event event;
+	int ret;
+
+	ret = k_msgq_get(&data->event_msgq, &event, K_NO_WAIT);
+	if (ret != 0) {
+		return;
+	}
+
+	/* Run the callback */
+	if (data->callback != NULL) {
+		data->callback(data->dev, event, data->event_cb_user_data);
+	}
+
+	/* If there are more pending events, resubmit this work item to handle them */
+	if (k_msgq_num_used_get(&data->event_msgq) > 0) {
+		k_work_submit(work);
+	}
+}
+#endif /* CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_GENERATE_ISR_SAFE_EVENTS */
 
 static void update_direction_from_step_count(const struct device *dev)
 {
-	const struct stepper_control_config *config = dev->config;
 	struct stepper_control_data *data = dev->data;
 
 	if (data->step_count > 0) {
 		data->direction = STEPPER_DIRECTION_POSITIVE;
-		stepper_set_direction(config->stepper, STEPPER_DIRECTION_POSITIVE);
 	} else if (data->step_count < 0) {
 		data->direction = STEPPER_DIRECTION_NEGATIVE;
-		stepper_set_direction(config->stepper, STEPPER_DIRECTION_NEGATIVE);
 	} else {
 		LOG_ERR("Step count is zero");
 	}
@@ -49,13 +111,14 @@ static void update_direction_from_step_count(const struct device *dev)
 
 static int z_stepper_control_move_by(const struct device *dev, int32_t micro_steps)
 {
+	const struct stepper_control_config *config = dev->config;
 	struct stepper_control_data *data = dev->data;
 
 	/** Todo: Introduce a stepper driver call to check if the motor is enabled, if stepper is
 	 * not enabled return -ECANCELED
 	 */
 
-	if (data->step_interval == 0) {
+	if (data->timing_data.microstep_interval_ns == 0) {
 		LOG_ERR("Step interval not set or invalid step interval set");
 		return -EINVAL;
 	}
@@ -63,8 +126,11 @@ static int z_stepper_control_move_by(const struct device *dev, int32_t micro_ste
 	K_SPINLOCK(&data->lock) {
 		data->run_mode = STEPPER_RUN_MODE_POSITION;
 		data->step_count = micro_steps;
+		config->timing_source->update(&config->timing_config, &data->timing_data,
+					      data->timing_data.microstep_interval_ns);
 		update_direction_from_step_count(dev);
-		(void)k_work_reschedule(&data->stepper_control_dwork, K_NO_WAIT);
+		stepper_set_direction(config->stepper, data->direction);
+		config->timing_source->start(&config->timing_config, &data->timing_data);
 	}
 	return 0;
 }
@@ -96,24 +162,27 @@ static int z_stepper_control_run(const struct device *dev, const enum stepper_di
 	K_SPINLOCK(&data->lock) {
 		data->run_mode = STEPPER_RUN_MODE_VELOCITY;
 		data->direction = direction;
+		config->timing_source->update(&config->timing_config, &data->timing_data,
+					      data->timing_data.microstep_interval_ns);
 		stepper_set_direction(config->stepper, direction);
-		(void)k_work_reschedule(&data->stepper_control_dwork, K_NO_WAIT);
+		config->timing_source->start(&config->timing_config, &data->timing_data);
 	}
 	return 0;
 }
 
 static int z_stepper_control_stop(const struct device *dev)
 {
+	const struct stepper_control_config *config = dev->config;
 	struct stepper_control_data *data = dev->data;
+	int ret;
 
-	K_SPINLOCK(&data->lock) {
-		(void)k_work_cancel_delayable(&data->stepper_control_dwork);
-
-		if (data->callback) {
-			data->callback(data->dev, STEPPER_CONTROL_EVENT_STOPPED,
-				       data->event_cb_user_data);
-		}
+	ret = config->timing_source->stop(&config->timing_config, &data->timing_data);
+	if (ret != 0) {
+		LOG_ERR("Failed to stop timing source: %d", ret);
+		return ret;
 	}
+	stepper_trigger_callback(dev, STEPPER_CONTROL_EVENT_STOPPED);
+
 	return 0;
 }
 
@@ -140,6 +209,7 @@ static int z_stepper_control_get_actual_position(const struct device *dev, int32
 static int z_stepper_control_set_step_interval(const struct device *dev,
 					       uint64_t microstep_interval_ns)
 {
+	const struct stepper_control_config *config = dev->config;
 	struct stepper_control_data *data = dev->data;
 
 	if (microstep_interval_ns == 0) {
@@ -148,7 +218,9 @@ static int z_stepper_control_set_step_interval(const struct device *dev,
 	}
 
 	K_SPINLOCK(&data->lock) {
-		data->step_interval = microstep_interval_ns;
+		data->timing_data.microstep_interval_ns = microstep_interval_ns;
+		config->timing_source->update(&config->timing_config, &data->timing_data,
+					      microstep_interval_ns);
 	}
 	LOG_DBG("Setting Motor step interval to %llu", microstep_interval_ns);
 	return 0;
@@ -156,9 +228,10 @@ static int z_stepper_control_set_step_interval(const struct device *dev,
 
 static int z_stepper_control_is_moving(const struct device *dev, bool *is_moving)
 {
+	const struct stepper_control_config *config = dev->config;
 	struct stepper_control_data *data = dev->data;
 
-	*is_moving = k_work_delayable_is_pending(&data->stepper_control_dwork);
+	*is_moving = config->timing_source->is_running(&data->timing_data);
 	LOG_DBG("Motor is %s moving", *is_moving ? "" : "not");
 	return 0;
 }
@@ -190,18 +263,17 @@ static void position_mode_task(const struct device *dev)
 	const struct stepper_control_config *config = dev->config;
 	struct stepper_control_data *data = dev->data;
 
-	update_remaining_steps(dev);
 	stepper_step(config->stepper);
 	update_actual_position(dev);
-	if (data->step_count) {
-		(void)k_work_reschedule(&data->stepper_control_dwork, K_NSEC(data->step_interval));
-	} else {
-		if (data->callback) {
-			data->callback(dev, STEPPER_CONTROL_EVENT_STEPS_COMPLETED,
-				       data->event_cb_user_data);
-		}
-		(void)k_work_cancel_delayable(&data->stepper_control_dwork);
+
+	if (config->timing_source->needs_reschedule(dev) && data->step_count != 0) {
+		config->timing_source->start(&config->timing_config, &data->timing_data);
+	} else if (data->step_count == 0) {
+		stepper_trigger_callback(data->dev, STEPPER_CONTROL_EVENT_STEPS_COMPLETED);
+		config->timing_source->stop(&config->timing_config, &data->timing_data);
 	}
+
+	update_remaining_steps(dev);
 }
 
 static void velocity_mode_task(const struct device *dev)
@@ -211,7 +283,10 @@ static void velocity_mode_task(const struct device *dev)
 
 	stepper_step(config->stepper);
 	update_actual_position(dev);
-	(void)k_work_reschedule(&data->stepper_control_dwork, K_NSEC(data->step_interval));
+
+	if (config->timing_source->needs_reschedule(dev)) {
+		(void)config->timing_source->start(&config->timing_config, &data->timing_data);
+	}
 }
 
 static int z_stepper_control_set_mode(const struct device *dev, enum stepper_control_mode mode)
@@ -220,27 +295,6 @@ static int z_stepper_control_set_mode(const struct device *dev, enum stepper_con
 		return -ENOTSUP;
 	}
 	return 0;
-}
-
-static void stepper_control_work_step_handler(struct k_work *work)
-{
-	struct k_work_delayable *dwork = k_work_delayable_from_work(work);
-	struct stepper_control_data *data =
-		CONTAINER_OF(dwork, struct stepper_control_data, stepper_control_dwork);
-
-	K_SPINLOCK(&data->lock) {
-		switch (data->run_mode) {
-		case STEPPER_RUN_MODE_POSITION:
-			position_mode_task(data->dev);
-			break;
-		case STEPPER_RUN_MODE_VELOCITY:
-			velocity_mode_task(data->dev);
-			break;
-		default:
-			LOG_WRN("Unsupported run mode %d", data->run_mode);
-			break;
-		}
-	}
 }
 
 static int z_stepper_control_set_event_callback(const struct device *dev,
@@ -256,18 +310,48 @@ static int z_stepper_control_set_event_callback(const struct device *dev,
 	return 0;
 }
 
+void stepper_handle_timing_signal(const struct device *dev)
+{
+	struct stepper_control_data *data = dev->data;
+
+	K_SPINLOCK(&data->lock) {
+		switch (data->run_mode) {
+		case STEPPER_RUN_MODE_POSITION:
+			position_mode_task(dev);
+			break;
+		case STEPPER_RUN_MODE_VELOCITY:
+			velocity_mode_task(dev);
+			break;
+		default:
+			LOG_WRN("Unsupported run mode: %d", data->run_mode);
+			break;
+		}
+	}
+}
+
 static int stepper_control_init(const struct device *dev)
 {
 	const struct stepper_control_config *config = dev->config;
 	struct stepper_control_data *data = dev->data;
+	int ret;
 
 	data->dev = dev;
-	if (!device_is_ready(config->stepper)) {
-		LOG_ERR("Stepper device %s is not ready", config->stepper->name);
-		return -ENODEV;
+
+	if (config->timing_source->init) {
+		ret = config->timing_source->init(&config->timing_config, &data->timing_data);
+		if (ret < 0) {
+			LOG_ERR("Failed to initialize timing source: %d", ret);
+			return ret;
+		}
 	}
+#ifdef CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_GENERATE_ISR_SAFE_EVENTS
+
+	k_msgq_init(&data->event_msgq, data->event_msgq_buffer, sizeof(enum stepper_control_event),
+		    CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_EVENT_QUEUE_LEN);
+	k_work_init(&data->event_callback_work, stepper_work_event_handler);
+#endif /* CONFIG_ZEPHYR_STEPPER_MOTION_CONTROL_GENERATE_ISR_SAFE_EVENTS */
+
 	LOG_DBG("Stepper Control initialized for stepper driver %s", config->stepper->name);
-	k_work_init_delayable(&data->stepper_control_dwork, stepper_control_work_step_handler);
 	return 0;
 }
 
@@ -286,10 +370,16 @@ static DEVICE_API(stepper_control, stepper_control_api) = {
 
 #define STEPPER_CONTROL_DEFINE(inst)                                                               \
 	static const struct stepper_control_config stepper_control_config_##inst = {               \
-		.stepper = DEVICE_DT_GET(DT_INST_PHANDLE(inst, stepper)),                          \
+		.stepper = DEVICE_DT_GET(DT_INST_CHILD(inst, stepper)),                            \
+		.timing_config.counter =                                                           \
+			DEVICE_DT_GET_OR_NULL(DT_PHANDLE(DT_DRV_INST(inst), counter)),             \
+		.timing_source = COND_CODE_1(DT_INST_NODE_HAS_PROP(inst, counter),                 \
+			(&step_counter_timing_source_api), (&step_work_timing_source_api)),        \
 	};                                                                                         \
 	struct stepper_control_data stepper_control_data_##inst = {                                \
-		.step_interval = DT_INST_PROP(inst, step_tick_ns),                                 \
+		.timing_data.microstep_interval_ns = DT_INST_PROP(inst, step_tick_ns),             \
+		.timing_data.motion_control_dev = DEVICE_DT_GET(DT_DRV_INST(inst)),                \
+		.timing_data.stepper_handle_timing_signal_cb = stepper_handle_timing_signal,       \
 	};                                                                                         \
 	DEVICE_DT_INST_DEFINE(inst, stepper_control_init, NULL, &stepper_control_data_##inst,      \
 			      &stepper_control_config_##inst, POST_KERNEL,                         \
